74ee15684b9ec393c07055c53b7802a3c946dcbc
testdevops2017 <34516353+testdevops2017@users.noreply.github.com>
2018-01-08 22:44:02 +0300

    Update Main.java

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -4,9 +4,12 @@
  *
  */
  
+package com.mycompany.app;
 
 public class Main 
 {
+ 
+ 
     public static void main( String[] args )throws Exception
     {
     	while(true){
@@ -14,4 +17,30 @@ public class Main
         	//System.out.println("saysaysay");
         }
     }
+ 
+    public int sum(int a, int b) {
+    return a+b;
+    }
+
+    public int sub(int a, int b) {
+    return a-b;
+    }
+
+    public int mult(int a, int b) {
+    return a*b;
+    }
+ 
+    public double sumDouble (double a, double b){
+    return a + b ;
+    }
+ 
+    public String retString(String a){
+    return a;
+    }
+ 
+    public String StringAdder(String a,String b){
+    return a+b;
+    }
+ 
+ 
 }
